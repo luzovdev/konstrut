32b019fb3dd7991f5b6697a56b8ca89dba924d21
luzovdev <86262718+luzovdev@users.noreply.github.com>
2021-06-29 13:23:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function ibg() {
 
-   let ibg = document.querySelectorAll(".ibg");
-   for (var i = 0; i < ibg.length; i++) {
-      if (ibg[i].querySelector('img')) {
-         ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+   $.each($('.ibg'), function (index, val) {
+      if ($(this).find('img').length > 0) {
+         $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
       }
-   }
+   });
 }
+
 ibg();
 
 //goto
@@ -17,6 +17,8 @@ $('.goto').click(function () {
 
    return false;
 });
+
+
 
 
 
